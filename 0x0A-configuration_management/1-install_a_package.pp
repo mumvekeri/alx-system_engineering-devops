4c3a3f7850d72ef4609fc502641776a0435425c4
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-19 10:24:57 -0800

    try

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,19 @@
 #!/usr/bin/pup
 # puppet apply 1-install_a_package.pp
+
 package {'python3-pip':
   ensure => installed,
 }
 
 package {'flask':
-  ensure => '2.1.0',
-  provider => 'pip'
-  require => Package['python3-pip'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
 package {'werkzeug':
-  ensure => '2.0.1',
-  provider => 'pip'
-  require => Package['python3-pip'],
+  ensure   => '2.0.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
