5ad5520cd553f3b258c8aa61a4913d573f041f71
mumvekeri <mumvekerichidochashe@gmail.com>
2024-01-19 10:12:51 -0800

    try

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,21 @@
 #!/usr/bin/pup
 # installs puppet-lint when run
 
-package { 'puppet-lint':
-    ensure   => '2.1.1',
-    provider => 'gem',
+# File: site.pp
+
+# Ensure Python is installed
+package { 'python3':
+  ensure => present,
+}
+
+# Ensure pip is installed
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => [Package['python3'], Package['python3-pip']],
 }
