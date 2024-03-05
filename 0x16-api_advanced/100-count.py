11e73d596558eefa770ff0c0111a364fe4581249
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-03-05 21:04:38 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""A file to make a query to an endpoint"""
-from requests import request
+""" recursive function that queries the Reddit API"""
+import requests
+import sys
+after = None
+count_dic = []
 
 
-def count_words(subreddit, word_list, after="", counter={}, ini=0):
-    """A recursive function that queries the Reddit API, parses the title
-    of all hot articles, and prints a sorted count of given keywords.
-    """
-    if ini == 0:
-        for word in word_list:
-            counter[word] = 0
-
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    response = request("GET", url, headers={"User-Agent": "Python3"}).json()
-    try:
-        top = response['data']['children']
-        _after = response['data']['after']
-        for item in top:
-            for word in counter:
-                counter[word] += item['data']['title'].lower(
-                    ).split(' ').count(word.lower())
-        if _after is not None:
-            count_words(subreddit, word_list, _after, counter, 1)
-        else:
-            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-            for name, num in str:
-                if num != 0:
-                    print('{}: {}'.format(name, num))
-    except Exception:
-        return None
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
