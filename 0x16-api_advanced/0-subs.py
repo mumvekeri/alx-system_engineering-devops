8754ec79d26919034113eccc0783d6a8e3cb14f0
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-03-05 13:44:28 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and returns the number of
-subscribers for a given subreddit"""
+"""
+ A function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    if results is None:
-        return 0
-    return results.get("subscribers")
+    """ Return the total number of subscribers. """
+    sub = requests.get("https://reddit.com/r/{}/about.json".format(subreddit),
+                       headers={"User-Agent": "Custom"})
+
+    if (sub.status_code == 200):
+        return sub.json().get("data").get("subscribers")
+    else:
+        return (0)
