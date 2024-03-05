bfa5d65e49930534eb3d78d02d6be151300c0c70
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-03-05 13:26:37 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
 import requests
 
-def recurse(subreddit, hot_list=[]):
-    """
-    Recursively retrieve the titles of all hot articles for a given subreddit.
-    """
+def add_title(hot_list, hot_posts):
+    """ Add item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Query Reddit API """
+    headers = {
+        'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
+    }
+    params = {
+        'after': after
+    }
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
-    params = {'limit': 100, 'after': hot_list[-1]['data']['name']} if hot_list else {'limit': 100}
-    
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        
-        # Check if the subreddit is invalid
-        if response.status_code == 404:
-            return None
-        
-        # Parse the JSON response
-        data = response.json().get('data', {}).get('children', [])
-        
-        # Append current page's hot posts to the list
-        hot_list.extend(data)
-        
-        # Check if there are more pages and recursively call the function
-        if data and data[-1]['data']['name'] != hot_list[-1]['data']['name']:
-            recurse(subreddit, hot_list)
-        
-        return hot_list
-    
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if res.status_code != 200:
         return None
+
+    body = res.json()
+    hot_posts = body['data']['children']
+    add_title(hot_list, hot_posts)
+    after = body['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
 
 # Example usage:
 subreddit_name = "python"
 result = recurse(subreddit_name)
 if result is not None:
-    for post in result:
-        print(post['data']['title'])
+    for post_title in result:
+        print(post_title)
