6251713591fcca1315fc07ff8cc56d5217755f4a
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-03-05 12:46:19 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,7 @@
 """a function that queries the Reddit API and returns the number of
 subscribers for a given subreddit"""
 import requests
-from json.decoder import JSONDecodeError  # Import JSONDecodeError
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
@@ -10,15 +10,10 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        results = response.json().get("data")
-        return results.get("subscribers")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-subreddit_name = "python"
-subscribers_count = number_of_subscribers(subreddit_name)
-print(f"The number of subscribers for r/{subreddit_name} is: {subscribers_count}")
+    results = response.json().get("data")
+    if results is None:
+        return 0
+    return results.get("subscribers")
